b485077ec8343ddbb751a1db7b1aa4aa6e68a62f
Lucas juliano <lucas.juliano@live.com>
2018-10-02 15:47:01 -0300

    mais de react

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native'
+import { StyleSheet, Text, View, FlatList, ActivityIndicator, KeyboardAvoidingView, TextInput, Dimensions } from 'react-native'
+import { Appbar, Button, Card, Title, Dialog, Paragraph, Portal } from 'react-native-paper';
 
 export default class App extends React.Component<any, any, any> {
 
+  _showDialog = () => this.setState({ visible: true });
+  _hideDialog = () => this.setState({ visible: false });
 
-  constructor(public props: any) {
-    super(props)
-    this.state = {
-      isLoadingComplete: false,
-    }
-  }
+  widthDevice = Dimensions.get('window').width;
 
-  async componentDidMount(): Promise<any> {
-    return await fetch('https://facebook.github.io/react-native/movies.json')
+  onClickLoadMovies = async () => {
+    this.setState({ isLoading: true });
+
+    await fetch('https://facebook.github.io/react-native/movies.json')
       .then((response) => response.json())
       .then((data: {
         title: string,
@@ -20,41 +20,104 @@ export default class App extends React.Component<any, any, any> {
         movies: [{ title: string, releaseYear: string }]
       }) => {
 
-        this.setState({
-          isLoading: false,
-          title: data.title,
-          description: data.description,
-          dataSource: data.movies,
-        }, function () {
 
-        });
+        setTimeout(() => {
 
-      })
-      .catch((error) => {
+          this.setState({
+            isLoading: false,
+            title: data.title,
+            description: data.description,
+            dataSource: data.movies,
+          }, function () {
+
+          });
+        }, 1000);
+
+
+      }).catch((error) => {
         console.error(error);
       });
+  }
+
+  onEditItem = (item: any) => {
+    alert(`Edit => ${item.title}`);
+  }
+
+  constructor(public props: any) {
+    super(props)
+    this.state = {
+      isLoadingComplete: false,
+      visible: false,
+      dataSource: []
+    };
+  }
+
+  async componentDidMount(): Promise<any> {
+    this.onClickLoadMovies();
   }
 
   render() {
 
     if (this.state.isLoading) {
       return (
-        <View style={{ flex: 1, padding: 30 }}>
-          <ActivityIndicator />
+        <View style={{ flex: 1, padding: 30, justifyContent: 'center' }}>
+          <ActivityIndicator size="large" color="#3498DB" />
+          <Text style={{ fontSize: 16, textAlign: 'center', marginTop: 10, color: '#222222', fontWeight: 'bold' }}>Carregando filmes.... </Text>
         </View>
       )
     }
 
+    if (this.state.dataSource.length === 0) {
+      return (
+
+        <View style={styles.container}>
+          <Title>Nenhum filme encontrado</Title>
+          <Button icon="sync"   color="#3498DB" mode="outlined" onPress={() => this.onClickLoadMovies()}>
+            Carregar filmes
+  </Button>
+        </View>
+      );
+    }
+
     return (
-      <View style={styles.container}>
-        <Text style={{ alignSelf: 'center', color: 'blue', fontWeight: '600', fontSize: 24 }}>{this.state.title} </Text>
-        <Text style={{ alignSelf: 'center', color: 'blue', fontWeight: '600', fontSize: 16 }}>{this.state.description} </Text>
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={(item: any) => <Text style={styles.item}>{item.title}, {item.releaseYear}</Text>}
-          keyExtractor={({ id }, index) => id}
-        />
-      </View>
+
+      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+        <View style={styles.container}>
+          <Title>Olá, Lucas Juliano</Title>
+          <FlatList
+            style={{ width: this.widthDevice, padding: 10 }}
+            data={this.state.dataSource}
+            renderItem={({ item }) =>
+              <Card style={{ marginBottom: 10 }}>
+                <Card.Content >
+                  <Title>#{item.id} - {item.title}</Title>
+                  <Paragraph>{item.releaseYear}</Paragraph>
+
+                </Card.Content>
+                {/* <Card.Cover source={{ uri: 'https://picsum.photos/700' }} /> */}
+                <Card.Actions>
+                  <Button color="red" icon="delete" mode="text" onPress={() => {
+                    this.setState({ ...this.state.dataSource.splice({ item }, 1) });
+                  }}>REMOVER</Button>
+                  <Button color="#3498DB" icon="edit" mode="text" onPress={() => { this.onEditItem(item) }}>EDITAR</Button>
+                </Card.Actions>
+
+
+              </Card>
+            }
+            keyExtractor={({ id }, index) => id}
+          />
+
+          <Appbar style={{ width: this.widthDevice, backgroundColor: "#3498DB" }}>
+            <Appbar.Action color="#fff" icon="archive" onPress={() => console.log('Pressed archive')} />
+            <Appbar.Action color="#fff" icon="mail" onPress={() => console.log('Pressed mail')} />
+            <Appbar.Action color="#fff" icon="label" onPress={() => console.log('Pressed label')} />
+            <Appbar.Action color="#fff" icon="delete" onPress={() => console.log('Pressed delete')} />
+          </Appbar>
+        </View>
+      </KeyboardAvoidingView>
+
+
     );
   }
 }
@@ -69,6 +132,8 @@ const styles = StyleSheet.create({
   }, item: {
     padding: 10,
     fontSize: 18,
-    height: 44,
+    height: 44
+  }, bottom: {
+
   },
 })
